b1115c8959db3605bf27bfbaa8b32e53364171f1
Edoardo-Ferrante <eferrante@tylostudios.com>
2023-02-28 12:07:27 +0000

    Updated

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.use(express.static('assets'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(port, () => {
+    console.log("Website is live at http://localhost:"+port);
+});
